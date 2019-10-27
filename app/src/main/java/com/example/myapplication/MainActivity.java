25a0905dd31b6e218e70d2e57117b9aa09278dd7
Vahid Taatefi <vahid2264>
2019-10-27 15:55:35 +0330

    chg for recycleview and drawer navigation

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recycle = findViewById(R.id.recycler);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Profile");
+        list.add("Caller");
+        ImageButton navbar = findViewById(R.id.navbar);
+        navbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawerLayout = findViewById(R.id.draw);
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        recycleadapter adapter = new recycleadapter(list);
+        recycle.setAdapter(adapter);
+        recycle.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false));
+
+        //   Button btnprofile=findViewById(R.id.btnprofile);
+        //   Button btncall=findViewById(R.id.btncall);
+        //   btnprofile.setOnClickListener(new View.OnClickListener() {
+        //       @Override
+        //       public void onClick(View view) {
+        //           Intent i=new Intent(MainActivity.this,profileset.class);
+        //           startActivity(i);
+
+        //   });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/recycleadapter.java
@@ -0,0 +1,76 @@
+package com.example.myapplication;
+
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class recycleadapter extends RecyclerView.Adapter<recycleadapter.recyclerholder>{
+    ArrayList<String> myList;
+
+    public recycleadapter(ArrayList list) {
+        myList = list;
+    }
+
+    @NonNull
+    @Override
+    public recyclerholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        recyclerholder holder=new recyclerholder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final recyclerholder holder, int position) {
+       // holder.txtName.setText(myList.get(position));
+        holder.img.setText(myList.get(position));
+        holder.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent;
+                if(holder.img.getText()=="Profile")
+                {
+                    intent = new Intent(v.getContext(),profileset.class);
+
+                }else
+                {
+                    intent=new Intent(v.getContext(),phone.class);
+                }
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+//      if (position==0)
+//          holder.img.setBackgroundResource(R.drawable.profile);
+//      else
+//          holder.img.setBackgroundResource(R.drawable.call);
+    }
+
+    @Override
+    public int getItemCount() {
+        return myList.size();
+    }
+
+    class recyclerholder extends RecyclerView.ViewHolder{
+        TextView txtName;
+        Button img;
+
+        public recyclerholder(@NonNull View itemView) {
+            super(itemView);
+          //  txtName = itemView.findViewById(R.id.txtName);
+            img=itemView.findViewById(R.id.img);
+
+        }
+    }
+}
